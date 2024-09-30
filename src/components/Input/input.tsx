876015b25f2e0afadfd795e8dbfaef2845818825
GustavoBento <gustavo_grb-@hotmail.com>
2024-09-30 20:56:56 -0300

    testes

--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -10,6 +10,6 @@ export default function Input({type, placeholder, maxlength}: InputProps) {
 
       
   return (
-      <input type={type} className=" p-3 bg-input w-100 border-0 rounded-3 fs-5" placeholder={placeholder} max-length={maxlength}  required />
+      <input type={type} name="message" className=" p-3 bg-input w-100 border-0 rounded-3 fs-5" placeholder={placeholder} max-length={maxlength}  required />
   );
 }
